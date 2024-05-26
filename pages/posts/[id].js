40d272a12ac4a85bb55087e99d3153890c42dc07
Caroline Bates <caroline.portilla@gmail.com>
2024-05-26 16:45:54 +0800

    add images to blog

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,13 +4,10 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date2 from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
 import Comments from "../../components/Comments/comments";
-import { useEffect, useState} from "react";
-
+import { useEffect, useState } from "react";
 
 export async function getStaticProps({ params }) {
-  // Add the "await" keyword like this:
   const postData = await getPostData(params.id);
-
   return {
     props: {
       postData,
@@ -30,16 +27,23 @@ export default function Post({ postData }) {
   const [htmlContent, setHtmlContent] = useState('');
 
   useEffect(() => {
-
-    // Manipulate the HTML content to add target="_blank" to all anchor tags
-    const modifiedHtml = postData.contentHtml.replace(/<a /g, '<a target="_blank" ');
+    // Modify the HTML content to add target="_blank" to all anchor tags
+    const modifiedHtml = postData.contentHtml
+      .replace(/<a /g, '<a target="_blank" ')
+      .replace(
+        '<p><img',
+        `<div class=${utilStyles.imageContainer}><img`
+      )
+      .replace(
+        'wattle"></p>',
+        'wattle"></div>'
+      );
 
     setHtmlContent(modifiedHtml);
-  }, []);
+  }, [postData.contentHtml]);
 
   return (
     <Layout>
-      {/* Add this <Head> tag */}
       <Head>
         <title>{postData.title}</title>
       </Head>
@@ -49,7 +53,7 @@ export default function Post({ postData }) {
           <Date2 dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-        <Comments postData={postData}/>
+        <Comments postData={postData} />
       </article>
     </Layout>
   );
--- posts/seo.md
+++ posts/seo.md
@@ -5,4 +5,6 @@ date: '2024-05-10'
 
 So I've been working on a good friend's Squarespace website [TracyRobinsonCreate](https://tracyrobinsoncreate.com.au). She is a talented artist based in Fremantle, Western Australia. He art is primarily modern floral art using bright bold colours and native Australian flowers. The paintings are gorgeous. I have a couple. 
 
+![Serene Floral Art using bright colours](/images/serenefloralart.jpeg) ![TracyRobinsonCreate Business Card for Botanical Artist](/images/Business+Card.jpg) ![Artist Tracy Robinson with painting of native Australian wattle](/images/tracyrobinsonphoto.jpeg)
+
 Having recently being made redundant, I have a bit of time on my hands and have been working on improving the website's SEO and improving the mobile responsiveness of the site. SEO is a big rabbit hole to go down and I wonder if everyone following all the best practice strategies will diminish the effectiveness of all the recommendations given by various website audits. Food for thought!
